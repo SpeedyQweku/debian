25fc70d7f736b5aa772b7d0c0716137377e4f0bc
SpeedyQweku <princealex2024@gmail.com>
2022-12-05 02:50:41 +0000

    commit

--- wm/bsp.sh
+++ wm/bsp.sh
@@ -21,6 +21,8 @@ sudo systemctl enable acpid
 
 sudo apt install -y exa
 
+sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
+
 sudo apt install -y network-manager network-manager-gnome 
 
 sudo apt install -y cups
