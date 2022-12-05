00706e13bd8b89c77fdcb05a5c2547a33f718b48
SpeedyQweku <princealex2024@gmail.com>
2022-12-05 03:04:13 +0000

    commit

--- min_deb/android.sh
+++ min_deb/android.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 sudo apt install adb fastboot android-sdk-libsparse-utils android-sdk-platform23 android-sdk-build-tools android-sdk-common android-sdk-platform-tools -yy
+
+sleep 1
+
+sudo apt install ideviceinstaller libimobiledevice-utils python-imobiledevice libimobiledevice6 libplist3 libplist-utils python-plist ifuse usbmuxd libusbmuxd-tools gvfs-backends gvfs-bin gvfs-fuse -yy
+
+sudo echo "user_allow-other" >> /etc/fuse.conf
+
+sudo usermod -aG fuse $User
