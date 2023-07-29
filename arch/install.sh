dc185e6a8141b929b739dea387fcc78edf6e84c0
SpeedyQweku <princealex2024@gmail.co>
2023-07-29 13:19:15 +0000

    acrh

---
+++ arch/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+sudo pacman -Syu visual-studio-code-bin nordvpn-bin brave-bin ufw \
+    tlp nmap whois tor macchanger htop ncdu chromium remmina qbittorrent python-pip \
+    anydesk-bin gparted  nim php steghide scrcpy reaver crunch aircrack-ng \
+    jre17-openjdk insomnia kotlin \
+    ninja ninja cmake nvm vmware-workstation  \
+    ttf-ms-fonts  unrar adb android-sdk-build-tools  wireshark-qt  \
+    networkmanager-openvpn networkmanager-pptp networkmanager-strongswan \
+    networkmanager-vpnc dsniff zip ettercap-gtk  boost  \
+    libnetfilter_queue obs-studio burpsuite locate \
+    unzip wget nvm zoom pacman-contrib  nodejs  xclip \
+    ripgrep npm shotwell  gcc clang   yay  gpick  chafa\
+    pyright btop  tk qt5-webengine qt5-networkauth \
+    qt5-websockets qt5-script qt5-speech fd ripgrep \
+    bleachbit bashtop onlyoffice-bin antiword docx2txt \
+    gdb gdb tmux assetfinder  amass httprobe  \
+    httprobe  dnsvalidator subfinder netdiscover  sublist3r \
+    openssh ffuf cmatrix neofetch linux-lts linux-lts-headers \
+    hashcat john ruby john masscan fatresize \
+    lvm2 lib32-e2fsprogs dosfstools gotop sslstrip \
+    driftnet  sslstrip dsniff driftnet  go  dirbuster  seclists  \
+    ffuf  gobuster  diffutils  ranger  screenfetch  \
+    xorg-xbacklight vim xcb-util  xcb-util-keysyms  xcb-util-wm \
+    sublime-text-4  ffuf  gobuster  pwncat-caleb  mpv  \
+    conky svn jq powerpill arduino fortune-mod  \
+    findomain   tlsx favfreak  httpx \
+    notesnook-bin microsoft-edge-stable-bin  awesome  paru  \
+    cero  eyewitness  nikto  reflector \
+    teamviewer  silos  networkmanager-openvpn  \
+    filezilla openbsd-netcat  traceroute  bind  pwncat  chromium  exa  \
+    wifite  wifiphisher   wifiphisher  fzf  tldr  gnome-shell-extension-gtile  \
+    python-pipx  yt-dlp  gcc-fortran  dnsx  net-tools  gau  signal-desktop  dalfox  gf
